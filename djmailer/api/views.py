f2bf6d4d575e69823d40f1ce53f5ff04a39bf7f3
Fanner487 <eamontang1@hotmail.com>
2017-12-27 15:28:23 +0000

    first

--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -10,21 +10,28 @@ from django.shortcuts import render
 # Create your views here.
 
 
-# class HelloWorldView(APIView):
-# 	# This method is overwritten
-# 	def get(self, request):
-# 		return Response({"message": "Hello world!"})
+class HelloWorldView(APIView):
+	# This method is overwritten
+	def get(self, request):
+		return Response({"message": "Hello world!"})
 
-@api_view(["GET", "POST"])
-def hello_world(request):
-	if request.method == "GET":
-		return Response({"message": "Hello World!"})
-
-	else:
+	def post(self, request):
 		name = request.data.get("name")
 
 		if not name:
 			return Response({"error": "No name passed"})
-		else:
-			return Response({"message": "Hello {}!".format(name)})
-			
+		return Response({"message": "Hello {}!".format(name)})
+
+# @api_view(["GET", "POST"])
+# def hello_world(request):
+# 	if request.method == "GET":
+# 		return Response({"message": "Hello World!"})
+
+# 	else:
+# 		name = request.data.get("name")
+
+# 		if not name:
+# 			return Response({"error": "No name passed"})
+# 		else:
+# 			return Response({"message": "Hello {}!".format(name)})
+
