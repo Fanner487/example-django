9390c4de55b2763d52bbb7e42cfa8ea9322a451b
Fanner487 <eamontang1@hotmail.com>
2017-12-27 14:47:13 +0000

    first

--- djmailer/djmailer/settings.py
+++ djmailer/djmailer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rgra9jj1!kvo4rdi#&2+6w0#c8i^%*dmfi*-z)6br!q05sor#!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'djmailer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
