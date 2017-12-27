bd579d4219cbf126a2c427027b6b4e3dd30cde1c
Fanner487 <eamontang1@hotmail.com>
2017-12-27 15:37:54 +0000

    first

---
+++ djmailer/api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class HelloWorldSerializer(serializers.Serializer):
+	name = serializers.CharField(required=True, max_length=6)
+	age = serializers.IntegerField(required=False, min_value=10, default=10)
--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -4,6 +4,7 @@ from django.http.response import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import HelloWorldSerializer
 
 from django.shortcuts import render
 
@@ -16,11 +17,25 @@ class HelloWorldView(APIView):
 		return Response({"message": "Hello world!"})
 
 	def post(self, request):
-		name = request.data.get("name")
 
-		if not name:
-			return Response({"error": "No name passed"})
-		return Response({"message": "Hello {}!".format(name)})
+		serializer = HelloWorldSerializer(data=request.data)
+
+		if serializer.is_valid():
+			
+			valid_data = serializer.data
+
+			name = valid_data.get("name")
+			age = valid_data.get("age")
+
+			return Response({"message": "Hello {}, you're {} years old".format(name, age)})
+		else:
+			return Response({"errors": serializer.errors})
+
+		# name = request.data.get("name")
+
+		# if not name:
+		# 	return Response({"error": "No name passed"})
+		# return Response({"message": "Hello {}!".format(name)})
 
 # @api_view(["GET", "POST"])
 # def hello_world(request):
