087d61f795331c4dfaeb12af46bfdb1cef3e58d6
Fanner487 <eamontang1@hotmail.com>
2017-12-27 15:06:08 +0000

    first

--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.http.response
 
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def hello_world(request):
+	return JsonResponse({"message": "Hello world!"})
--- djmailer/djmailer/urls.py
+++ djmailer/djmailer/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^api/', include(api_urls, namespace="api")),
     url(r'^admin/', admin.site.urls),
 ]
