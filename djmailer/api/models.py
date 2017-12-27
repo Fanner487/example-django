09591836862b42f07a71edd2cc21baf8cf6460e9
Fanner487 <eamontang1@hotmail.com>
2017-12-27 15:45:40 +0000

    first

--- djmailer/api/models.py
+++ djmailer/api/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Subscriber(models.Model):
+	name = models.CharField("Name", max_length=50)
+	age = models.IntegerField("Age")
+	email = models.EmailField("Email")
