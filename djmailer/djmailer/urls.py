a99633f00a0b4f717a66f8b2e02b093e1a0e7b73
Fanner487 <eamontang1@hotmail.com>
2017-12-27 15:07:44 +0000

    first

--- djmailer/djmailer/urls.py
+++ djmailer/djmailer/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from api import urls as api_urls
+
 urlpatterns = [
 	url(r'^api/', include(api_urls, namespace="api")),
     url(r'^admin/', admin.site.urls),
