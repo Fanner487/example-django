2b48768dca7abd4cb86aa510f9fbfa1d1c37aa58
Fanner487 <eamontang1@hotmail.com>
2017-12-27 18:07:53 +0000

    first

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import url
+from rest_framework.routers import SimpleRouter
 
 from .views import SubscriberView
+from .views import SubscriberViewSet
 # from .views import hello_world
 
-urlpatterns = [
-    # url(r'^hello', hello_world, name="hello_world")
-    url(r'^hello', SubscriberView.as_view(), name="subsriber")
-    # url(r'^hello', hello_world, name="hello_world")
-]
+router = SimpleRouter()
+router.register("subsribers", SubscriberViewSet)
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     # url(r'^hello', hello_world, name="hello_world")
+#     url(r'^hello', SubscriberView.as_view(), name="subsriber")
+#     # url(r'^hello', hello_world, name="hello_world")
+# ]
--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -4,38 +4,48 @@ from django.http.response import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from rest_framework.viewsets import ModelViewSet
 from .serializers import SubscriberSerializer
 from .models import Subscriber
+from rest_framework.generics import ListCreateAPIView
 
 from django.shortcuts import render
 
 # Create your views here.
 
+class SubscriberViewSet(ModelViewSet):
 
-class SubscriberView(APIView):
-	# This method is overwritten
-	def get(self, request):
-		all_subscribers = Subscriber.objects.all()
-		serialized_subscribers = SubscriberSerializer(all_subscribers, many=True)
-		return Response(serialized_subscribers.data)
+	serializer_class = SubscriberSerializer
+	queryset = Subscriber.objects.all()
 
-	def post(self, request):
+	
 
-		serializer = SubscriberSerializer(data=request.data)
+# class SubscriberView(ListCreateAPIView):
 
-		if serializer.is_valid():
+
+# 	serializer_class = SubscriberSerializer
+# 	queryset = Subscriber.objects.all()
+
+
+	# # This method is overwritten
+	# def get(self, request):
+	# 	all_subscribers = Subscriber.objects.all()
+	# 	serialized_subscribers = SubscriberSerializer(all_subscribers, many=True)
+	# 	return Response(serialized_subscribers.data)
+
+	# def post(self, request):
+
+	# 	serializer = SubscriberSerializer(data=request.data)
+
+	# 	if serializer.is_valid():
 			
-			subscriber_instance = Subscriber.objects.create(**serializer.data)
+	# 		subscriber_instance = Subscriber.objects.create(**serializer.data)
 
-			return Response({"message": "Created subscriber {}".format(subscriber_instance.id)})
-		else:
-			return Response({"errors": serializer.errors})
+	# 		return Response({"message": "Created subscriber {}".format(subscriber_instance.id)})
+	# 	else:
+	# 		return Response({"errors": serializer.errors})
 
-		# name = request.data.get("name")
-
-		# if not name:
-		# 	return Response({"error": "No name passed"})
-		# return Response({"message": "Hello {}!".format(name)})
 
 # @api_view(["GET", "POST"])
 # def hello_world(request):
