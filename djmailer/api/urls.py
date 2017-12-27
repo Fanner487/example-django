522ed3915eedc6c32539f911d67618b5971d33c3
Fanner487 <eamontang1@hotmail.com>
2017-12-27 15:25:26 +0000

    first

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import HelloWorldView
+# from .views import HelloWorldView
+from .views import hello_world
 
 urlpatterns = [
     # url(r'^hello', hello_world, name="hello_world")
-    url(r'^hello', HelloWorldView.as_view(), name="hello_world")
+    # url(r'^hello', HelloWorldView.as_view(), name="hello_world")
+    url(r'^hello', hello_world, name="hello_world")
 ]
--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -3,14 +3,28 @@ from __future__ import unicode_literals
 from django.http.response import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from django.shortcuts import render
 
 # Create your views here.
 
 
-class HelloWorldView(APIView):
-	# This method is overwritten
-	def get(self, request):
-		return Response({"message": "Hello world!"})
+# class HelloWorldView(APIView):
+# 	# This method is overwritten
+# 	def get(self, request):
+# 		return Response({"message": "Hello world!"})
 
+@api_view(["GET", "POST"])
+def hello_world(request):
+	if request.method == "GET":
+		return Response({"message": "Hello World!"})
+
+	else:
+		name = request.data.get("name")
+
+		if not name:
+			return Response({"error": "No name passed"})
+		else:
+			return Response({"message": "Hello {}!".format(name)})
+			
