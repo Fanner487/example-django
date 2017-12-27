874724c35e82e604478422a3755f7866e9487abd
Fanner487 <eamontang1@hotmail.com>
2017-12-27 18:08:36 +0000

    first

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from rest_framework.routers import SimpleRouter
 
-from .views import SubscriberView
+# from .views import SubscriberView
 from .views import SubscriberViewSet
 # from .views import hello_world
 
