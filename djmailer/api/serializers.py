ea03dce2ca1b76485e59ffaf3e361bf8b9b6f965
Fanner487 <eamontang1@hotmail.com>
2017-12-27 17:54:56 +0000

    first

--- djmailer/api/serializers.py
+++ djmailer/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from .models import Subscriber
 
 class SubscriberSerializer(serializers.Serializer):
-	name = serializers.CharField(max_length=50)
-	age = serializers.IntegerField()
-	email = serializers.EmailField()
+	# name = serializers.CharField(max_length=50)
+	# age = serializers.IntegerField()
+	# email = serializers.EmailField()
+
+	class Meta:
+		model = Subscriber
+		fields = "__all__"
