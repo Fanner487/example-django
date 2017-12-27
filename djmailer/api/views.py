53117dd5c913bfbfc6d10730e0c64771f716cf95
Fanner487 <eamontang1@hotmail.com>
2017-12-27 22:17:22 +0000

    first

--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -19,7 +19,15 @@ class SubscriberViewSet(ModelViewSet):
 	serializer_class = SubscriberSerializer
 	queryset = Subscriber.objects.all()
 
-	
+	def create(self, request):
+		serializer = SubscriberSerializer(data=request.data)
+
+		if serializer.is_valid():
+			subscriber_instance = Subscriber.objects.create(**serializer.data)
+			return Response({"message": "Created subscriber {}".format(subscriber_instance.id)})
+		else:
+			return Response({"errors": serializer.errors})
+
 
 # class SubscriberView(ListCreateAPIView):
 
