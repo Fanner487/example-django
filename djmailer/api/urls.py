37e730c70b301b8253b1611d4af00a6cfba66ca7
Fanner487 <eamontang1@hotmail.com>
2017-12-27 15:17:12 +0000

    first

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from .views import hello_world
 
 urlpatterns = [
-    url(r'^hello', hello_world, name="hello_world")
+    # url(r'^hello', hello_world, name="hello_world")
+    url(r'^hello', HelloWorldView.as_view(), name="hello_world")
 ]
--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http.response import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def hello_world(request):
-	return JsonResponse({"message": "Hello world!"})
+class HelloWorldView(APIView):
+	# This method is overwritten
+	def get(self, request):
+		return Response({"message": "Hello world!"})
+
