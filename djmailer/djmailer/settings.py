4870e8450a391fb8364b434d154a67cd3f0377b8
Fanner487 <eamontang1@hotmail.com>
2018-01-23 13:15:11 +0000

    Added option to add first and last name

--- djmailer/djmailer/settings.py
+++ djmailer/djmailer/settings.py
@@ -118,6 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATETIME_FORMAT = '%d/%m/%Y %H:%M:%S'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
