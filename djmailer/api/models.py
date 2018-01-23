be37dd3e96bb871e603a95b812792b34514e7939
Fanner487 <eamontang1@hotmail.com>
2018-01-23 12:54:53 +0000

    Added option to add first and last name

--- djmailer/api/models.py
+++ djmailer/api/models.py
@@ -2,9 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Subscriber(models.Model):
 	name = models.CharField("Name", max_length=50)
 	age = models.IntegerField("Age")
 	email = models.EmailField("Email")
+	created = models.DateTimeField(auto_now_add=True)
+
+
+# class Event(models.Model):
+# 	organiser = models.CharField("organiser", max_length=50)
+# 	event_name = models.CharField("event_name", max_length=50)
+# 	location = models.CharField("location", max_length=50)
+# 	start_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+# 	finish_time = models.DateTimeField(auto_now=False, auto_now_add=False,)
+# 	attendees = ArrayField
+# 	attending = ArrayField
+# 	attendance_required = models 
