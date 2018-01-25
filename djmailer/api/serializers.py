b7a92f5b55fd3a834b7d1f00d378333aa3dcffe3
Fanner487 <eamontang1@hotmail.com>
2018-01-25 16:51:03 +0000

    changed timezone issues

--- djmailer/api/serializers.py
+++ djmailer/api/serializers.py
@@ -139,7 +139,8 @@ def user_is_attendee(username, event_id):
 
 			event = Event.objects.filter(id=event_id).filter(attendees__icontains=username.strip().lower())
 
-			if username.strip().lower() in event.attendees:
+			# if username.strip().lower() in event.attendees:
+			if event.exists()
 				print(username + " exists in " + event_id)
 			else:
 				print(username + " does not exists in " + event_id)
