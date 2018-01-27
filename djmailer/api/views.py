eb60f79b309eeb65a1181668a0ee5cc4b681bac5
Fanner487 <eamontang1@hotmail.com>
2018-01-27 13:50:33 +0000

    Array lookups don't query on exact strings, just contain

--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -134,7 +134,7 @@ def get_events(request, username, event_type):
 
 	elif event_type == "attending":
 
-		attending_events = Event.objects.filter(attendees__in=username)
+		attending_events = Event.objects.filter(attendees__contains=username)
 
 		serialized = EventSerializer(attending_events, many=True)
 
