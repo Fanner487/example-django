face89eb64c7c148e3427b11234beb8d368609f1
Fanner487 <eamontang1@hotmail.com>
2018-01-27 18:14:58 +0000

    Changing attempt model

--- djmailer/api/models.py
+++ djmailer/api/models.py
@@ -32,6 +32,6 @@ class Event(models.Model):
 class Attempt(models.Model):
 	username = models.CharField("username", max_length=50)
 	event_id = models.IntegerField("event_id")
-	time_created = models.DateTimeField(auto_now_add=True)
+	time_created = models.DateTimeField()
 	time_on_screen = models.DateTimeField()
 	date_on_screen = models.DateTimeField()
