7a94efcbe66c1293ac8bc76988fb33beb4efc84e
Fanner487 <eamontang1@hotmail.com>
2018-01-28 16:04:55 +0000

    added extra verification to attemp serializer

--- djmailer/api/serializers.py
+++ djmailer/api/serializers.py
@@ -204,9 +204,9 @@ def valid_attempt_in_event(username, event_id, time_on_screen, date_on_screen, t
 		verified = False
 
 	# Check screen time within timestamp delta
-	print(time_on_screen)
-	print(timestamp.time().strftime("%H:%M:%S"))
 	new_time_on_screen = time_on_screen + timezone.timedelta(milliseconds=0)
+	print(new_time_on_screen)
+	print(timestamp.time().strftime("%H:%M:%S"))
 	time_difference = (timestamp.time() - new_time_on_screen).total_seconds()
 
 	# (current_created - last_attempt.time_created).total_seconds()
