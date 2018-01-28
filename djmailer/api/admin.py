fe65d4fe6c94a5ff50439a88b538aa93dd629dc4
Fanner487 <eamontang1@hotmail.com>
2018-01-28 22:49:08 +0000

    changed time_created field to created

--- djmailer/api/admin.py
+++ djmailer/api/admin.py
@@ -7,7 +7,7 @@ from .models import Subscriber, Event, Attempt
 
 class AttemptAdmin(admin.ModelAdmin):
 	list_display = ('username', 'event_id', 'time_on_screen', 'date_on_screen')
-	readonly_fields = ('time_created',)
+	readonly_fields = ('created',)
 
 class EventAdmin(admin.ModelAdmin):
 	list_display = ('id', 'organiser', 'event_name', 'location', 'start_time', 'sign_in_time', 'finish_time')
