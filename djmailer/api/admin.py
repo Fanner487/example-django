4c4e1c0e07d2a64cf858c71a1aa7d4596e1652d3
Fanner487 <eamontang1@hotmail.com>
2018-01-28 18:42:07 +0000

    added extra verification to attemp serializer

--- djmailer/api/admin.py
+++ djmailer/api/admin.py
@@ -9,6 +9,10 @@ class AttemptAdmin(admin.ModelAdmin):
 	list_display = ('username', 'event_id', 'time_on_screen', 'date_on_screen')
 	readonly_fields = ('time_created',)
 
+class EventAdmin(admin.ModelAdmin):
+	list_display = ('id', 'organiser', 'name', 'location', 'start_time', 'sign_in_time', 'finish_time')
+	readonly_fields = ('time_created',)
+
 admin.site.register(Subscriber)
 admin.site.register(Event)
 admin.site.register(Attempt, AttemptAdmin)
